df1be736e13b21184ed2a49b59e2f8f80e540af9
Smithleystan <100232453+Smithleystan@users.noreply.github.com>
2022-05-19 12:20:24 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Nav from "./component/Navbar"
+import Foot from "./component/Footer"
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      
+     <div className="main">
+       
+     </div>
+     <Foot/>
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../App.css"
+
+function Navbar() {
+  return (
+    <div>
+        <div className='nav'>Navbar</div>
+    </div>
+  )
+}
+
+export default Navbar
