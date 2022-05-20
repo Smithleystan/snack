b7a1de93780fb22b8e704bcfd7183fefd346349d
Smithleystan <100232453+Smithleystan@users.noreply.github.com>
2022-05-20 21:16:41 +0200

    mas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import Nav from "./component/Navbar"
 import Foot from "./component/Footer"
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  
+  const [hey, setHey] = useState(false)
+ 
+ 
+
   return (
-    <div>
-      <Nav/>
+    <div> 
+     <div className={hey ? "hola" :" hole"}><h1>salut</h1></div>
+     <div className="test">
+      <button onClick ={()=>setHey(!hey)}>true</button>
       
-     <div className="main">
-       
      </div>
-     <Foot/>
+     <div className="main">
+     
+     </div>
+     
     </div>
   
   );
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 function Footer() {
+  
+
+
   return (
     <div>
         <div className='footer'>Footer</div>
